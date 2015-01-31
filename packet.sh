550c89b8556f79b9a4638e47dd3e16cea5a64655
zCirill <shulepovk@gmail.com>
2015-01-31 08:21:02 +0300

    init

---
+++ packet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then echo "set correct ip to docker service" && exit 1
+fi
+
+curl -sSL https://get.docker.com/ubuntu/ | sh
+
+echo DOCKER_OPTS="--bip=$1/16 --dns $1 --dns 8.8.8.8" >> /etc/default/docker
+
+service docker restart
---
+++ prepare.dns2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+#получаем адрес сервиса докера
+IP=`grep ^DOCKER_OPTS /etc/default/docker | cut -f3 -d" "`
+
+#установка dnsmasq
+apt-get update && apt-get install -y dnsmasq
+
+#конфигурация dnsmasq
+
+echo "nameserver 8.8.8.8" > /etc/resolv_dnsqmasq.conf
+
+echo -e "conf-dir=/etc/dnsmasq.d\nlisten-address=$IP\ninterface=docker0\nresolv-file=/etc/resolv_dnsqmasq.conf" > /etc/dnsmasq.conf
+
+echo -e "addn-hosts=/etc/docker-container-hosts\ninterface=docker0" > /etc/dnsmasq.d/docker
+
+touch /etc/docker-container-hosts
+
+echo "nameserver $IP" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+service dnsmasq restart
+
