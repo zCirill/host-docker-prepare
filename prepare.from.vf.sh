c0b27b5d24d106ca6e57f4f3c762e9981a1c14ad
zCirill <shulepovk@gmail.com>
2015-02-02 03:10:21 +0300

    hope its work ))

---
+++ prepare.from.vf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir /var/www
+mkdir /var/www/dev
+mkdir /var/www/stage
+mkdir /var/www/production
+
+chown ft-deploy.ft-deploy -R /var/www/
+chmod g+s -R /var/www/
+
+#ft-deploy ALL=NOPASSWD: /etc/init.d/nginx reload
+#ft-deploy ALL=NOPASSWD: /bin/chown -R ft-deploy /var/www/*
+
+
