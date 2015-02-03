fc496fce9ba27f957dd41ff57d495f440781f770
zCirill <shulepovk@gmail.com>
2015-02-04 01:05:01 +0300

    fix dns clear

--- dns-clear.sh
+++ dns-clear.sh
@@ -2,8 +2,9 @@
 
 docker ps -q | while read n; do docker inspect --format '{{ .Name }}' $n | cut -f2 -d"/"; done > docker.name
 
+rm /etc/docker-container-hosts
+touch /etc/docker-container-hosts
 
-cat docker.name | while read n; do IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $n` && echo $IP $n.ftvcd01 >> 
-/etc/docker-container-hosts; done
+cat docker.name | while read n; do IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $n` && echo $IP $n.ftvcd01 >> /etc/docker-container-hosts; done
 
 pkill -x -HUP dnsmasq
