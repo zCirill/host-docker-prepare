815f37bfc716eb5184c29531ce4c3677d960afe2
zCirill <shulepovk@gmail.com>
2015-02-17 00:38:00 +0300

    add local domain in dns clear

--- dns-clear.sh
+++ dns-clear.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+DOCKER_DOMAN=`hostname`
+
 docker ps -q | while read n; do docker inspect --format '{{ .Name }}' $n | cut -f2 -d"/"; done > docker.name
 
 rm /etc/docker-container-hosts
 touch /etc/docker-container-hosts
 
-cat docker.name | while read n; do IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $n` && echo $IP $n.ftvcd01 >> /etc/docker-container-hosts; done
+cat docker.name | while read n
+	do IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $n` && echo $IP $n.$DOCKER_DOMAN >> /etc/docker-container-hosts \
+	&& echo $IP $n.local >> /etc/docker-container-hosts
+done
 
 pkill -x -HUP dnsmasq
+
