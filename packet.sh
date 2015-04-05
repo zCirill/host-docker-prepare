7357eed94b13302e520fbeb55b49dfaf862a8567
zCirill <shulepovk@gmail.com>
2015-04-05 23:05:55 +0300

    Update packet.sh
    add curl to install

--- packet.sh
+++ packet.sh
@@ -3,6 +3,8 @@
 if [ -z "$1" ] ; then echo "set correct ip to docker service" && exit 1
 fi
 
+apt-get update && apt-get install curl
+
 curl -sSL https://get.docker.com/ubuntu/ | sh
 
 echo DOCKER_OPTS=\"--bip=$1/16 --dns $1 --dns 8.8.8.8\" > /etc/default/docker
