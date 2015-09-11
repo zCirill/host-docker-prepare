c8065f724a75b341ab6d5d90d7ee4a97e19d3ff2
zCirill <shulepovk@gmail.com>
2015-09-11 12:52:33 +0300

    Update local.prepare.sh
    add curl

--- local.prepare.sh
+++ local.prepare.sh
@@ -1,5 +1,8 @@
 sed -i s/ru.archive.ubuntu.com/mirror.yandex.ru/g /etc/apt/sources.list
 
+aptitude update
+aptitude install curl
+
 curl -sSL https://get.docker.com/ubuntu/ | sh
 
 echo DOCKER_OPTS=\"--bip=172.17.42.1/16 --dns 172.17.42.1 --dns 8.8.8.8\" > /etc/default/docker
